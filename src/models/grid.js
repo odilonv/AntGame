610d8f52b3cb830cf3f645ba0461c2751548a7b5
Raphael Dimeck <raphael.dimeck@gmail.com>
2024-01-23 00:45:55 +0100

    Refactor grid class and move constants to static properties

--- src/models/grid.js
+++ src/models/grid.js
@@ -5,11 +5,18 @@ import Agent from './agent.js';
 class Grid {
 
     static instance = null;
+    static _fps = 60; // Frame rate.
+    static size = 18;
+    static _speed = 1; // Nous voulons que 1 cellule (de notre grille) soit parcourue en 1 seconde (doit être dépendant des FPS fixés car la fonction est appelée à chaque frame). Notre unité de vitesse est donc "le nombre de cellules de la grille parcourues/seconde".
+    static _cellSize = 40; // La taille d'une cellule en pixel.
+    static canvas = document.getElementById('my_canvas');
+    static ctx = Grid.canvas.getContext('2d');
 
     constructor(size) {
         this.size = size;
         this.grid = [];
         this.directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
     }
 
     static getInstance(size) {
@@ -65,17 +72,11 @@ class Grid {
     }
 
     moveAnt(agent) {
-        let _fps = 60; // Frame rate.
-        let size = 18;
-        let _speed = 1; // Nous voulons que 1 cellule (de notre grille) soit parcourue en 1 seconde (doit être dépendant des FPS fixés car la fonction est appelée à chaque frame). Notre unité de vitesse est donc "le nombre de cellules de la grille parcourues/seconde".
         let moveOK = false;
         let previousX = agent.column;
         let previousY = agent.row;
-        let _cellSize = 40; // La taille d'une cellule en pixel.
-        let canvas = document.getElementById('my_canvas');
-        let ctx = canvas.getContext('2d');
 
-        ctx.clearRect(previousY * _cellSize + 20, previousX * _cellSize + 20, 20, 20); // Efface le canvas.
+        Grid.ctx.clearRect(previousY * Grid._cellSize + 20, previousX * Grid._cellSize + 20, 20, 20); // Efface le canvas.
 
         while (moveOK != true) {
             let whereIsNext;
@@ -86,11 +87,11 @@ class Grid {
             }
             let column = parseInt(agent.column);
             let row = parseInt(agent.row);
-            if (whereIsNext === 'down' && row < size - 1) { // en vrai on va vers la droite jsp pk
+            if (whereIsNext === 'down' && row < Grid.size - 1) { // en vrai on va vers la droite jsp pk
                 if (this.grid[column][row + 1].getType().toString() != "Obstacle") {
                     let _direction = 3 * (Math.PI / 2);
                     let dy = Math.sin(_direction) * -1;
-                    agent.row += dy * _speed / _fps;
+                    agent.row += dy * Grid._speed / Grid._fps;
                     moveOK = true;
                     if (parseInt(agent.row) > row) {
                         // this.grid[x][y].ants.pop(agent);
@@ -102,7 +103,7 @@ class Grid {
                 if (this.grid[column][row - 1].getType().toString() != "Obstacle" || agent.row > parseInt(agent.row) + 0.3) { // la case du dessus est libre ou la fourmi est sur le bord de la grille
                     let _direction = Math.PI / 2;
                     let dy = Math.sin(_direction) * -1;
-                    agent.row += dy * _speed / _fps;
+                    agent.row += dy * Grid._speed / Grid._fps;
                     moveOK = true;
                     if (parseInt(agent.row) < row) {
                         // this.grid[x][y].ants.pop(agent);
@@ -110,11 +111,11 @@ class Grid {
                     }
                 }
             }
-            else if (whereIsNext === 'right' && column < size - 1) { // en vrai on va vers le bas jsp pk
+            else if (whereIsNext === 'right' && column < Grid.size - 1) { // en vrai on va vers le bas jsp pk
                 if (this.grid[column + 1][row].getType().toString() != "Obstacle") {
                     let _direction = 0;
                     let dx = Math.cos(_direction);
-                    agent.column += dx * _speed / _fps;
+                    agent.column += dx * Grid._speed / Grid._fps;
                     moveOK = true;
                     if (parseInt(agent.column) > column) {
                         // this.grid[x][y].ants.pop(agent);
@@ -126,7 +127,7 @@ class Grid {
                 if (this.grid[column - 1][row].getType().toString() != "Obstacle" || agent.column > parseInt(agent.column) + 0.3) {
                     let _direction = Math.PI;
                     let dx = Math.cos(_direction);
-                    agent.column += dx * _speed / _fps; // On divise par les fps car la fonction est appelée selon un fps donné (#cellGrid/seconde).
+                    agent.column += dx * Grid._speed / Grid._fps; // On divise par les fps car la fonction est appelée selon un fps donné (#cellGrid/seconde).
                     moveOK = true;
                     if (parseInt(agent.column) < column) {
                         // this.grid[x][y].ants.pop(agent);
@@ -158,41 +159,27 @@ class Grid {
 
     displayAnts() {
         console.log(this.grid);
-        let canvas = document.getElementById('my_canvas');
-        let ctx = canvas.getContext('2d');
-        let _cellSize = 40; // La taille d'une cellule en pixel.
         let image = new Image();
         image.src = 'web/images/tiles/ant.png';
         // image.onload = () => {
         for (let i = 0; i < this.grid.length; i++) {
             for (let j = 0; j < this.grid[0].length; j++) {
-                if (this.grid[i][j].getType()== "Free") {
+                if (this.grid[i][j].getType() == "Free") {
                     for (let ant of this.grid[i][j].ants) {
-                        let x = ant.column * _cellSize;
-                        let y = ant.row * _cellSize;
+                        let x = ant.column * Grid._cellSize;
+                        let y = ant.row * Grid._cellSize;
 
-                        // rotate the image of the ant according to its direction
-                        ctx.save();
-
-                        // Translate to the center of the image
-                        ctx.translate(y + 30, x + 30);
-
-                        // Rotate the image based on the ant's direction
-                        ctx.rotate((ant.direction == 'up' ? 0 : ant.direction == 'right' ? Math.PI / 2 : ant.direction == 'down' ? Math.PI : 3 * Math.PI / 2));
-
-                        // Draw the image
-                        ctx.drawImage(image, -10, -10, 20, 20);
-
-                        // Restore the original transformation matrix
-                        ctx.restore();
+                        Grid.ctx.save();
+                        Grid.ctx.translate(y + 30, x + 30);
+                        Grid.ctx.rotate((ant.direction == 'up' ? 0 : ant.direction == 'right' ? Math.PI / 2 : ant.direction == 'down' ? Math.PI : 3 * Math.PI / 2));
+                        Grid.ctx.drawImage(image, -10, -10, 20, 20);
+                        Grid.ctx.restore();
                     }
                 }
             }
             // }
         };
     }
-
-
 }
 
 export default Grid;
